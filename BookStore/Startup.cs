e9aab4c914689ee44bcb499e904478b584705dfc
EZhitnikovich <ezhitnikovich@gmail.com>
2021-09-02 23:08:45 +0300

    add _Layout.cshtml, _ViewStart.cshtml

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -12,6 +12,8 @@ namespace BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
