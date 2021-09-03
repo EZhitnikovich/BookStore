c43c029d37cae20fbb48775a77281b1b005b0cc3
EZhitnikovich <ezhitnikovich@gmail.com>
2021-09-03 21:53:03 +0300

    add AppDbContext.cs

---
+++ BookStore/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+        
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
