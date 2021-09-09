dda53f4f30f92533cefe7f0fb9d17213299a11cd
EZhitnikovich <ezhitnikovich@gmail.com>
2021-09-09 22:43:03 +0300

    minor changes

--- BookStore/Data/Models/User.cs
+++ BookStore/Data/Models/User.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace BookStore.Data.Models
 {
     public class User : IdentityUser
     {
+        public List<Book> MarkedBooks { get; set; }
     }
 }
