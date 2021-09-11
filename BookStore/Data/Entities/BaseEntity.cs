46083ede4d4bba673bef3193858c32f9142c7ef9
EZhitnikovich <ezhitnikovich@gmail.com>
2021-09-11 22:29:14 +0300

    add Order.cs

--- BookStore/Data/Entities/BaseEntity.cs
+++ BookStore/Data/Entities/BaseEntity.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Data.Entities
 {
     public class BaseEntity : IEntity
     {
+        [Key]
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
---
+++ BookStore/Data/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using BookStore.Data.Entities;
+
+namespace BookStore.Data.Models
+{
+    public class Order: BaseEntity
+    {
+        public string Address { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public User User { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
