44d140fb4a1737c26365aa514326426c65cafc6b
EZhitnikovich <ezhitnikovich@gmail.com>
2021-10-03 14:52:38 +0300

    Add application context

---
+++ BookStore.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using BookStore.Domain.Auth;
+using BookStore.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Persistence
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ BookStore.Persistence/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using BookStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Persistence
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Category> Categories { get; set; }
+        DbSet<Book> Books { get; set; }
+        DbSet<Order> Orders { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
