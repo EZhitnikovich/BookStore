6efb4eae90af6ac827174180aee02c623a697264
EZhitnikovich <ezhitnikovich@gmail.com>
2021-12-09 22:23:15 +0300

    add search

---
+++ BookStore.Domain/ViewModels/SearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookStore.Domain.ViewModels
+{
+    public class SearchViewModel
+    {
+        public int CategoryId { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string BookName { get; set; }
+        
+        public int StartRating { get; set; }
+        
+        public int EndRating { get; set; }
+    }
+}
--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BookStore.Domain.Entities;
+using BookStore.Domain.ViewModels;
 using BookStore.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Controllers
 {
@@ -15,7 +21,31 @@ namespace BookStore.Controllers
         
         public IActionResult Index()
         {
-            return View(_context.Books.ToList());
+            var books = _context.Books.Include(x => x.Category).Include(m => m.Marks).ToList();
+
+            return View(books);
+        }
+        
+        [HttpPost]
+        public IActionResult Index(string bookName, float startRating, float endRating, DateTime startDate, DateTime endDate)
+        {
+            bookName = string.IsNullOrEmpty(bookName) ? string.Empty : bookName;
+            endDate = endDate == DateTime.MinValue ? DateTime.MaxValue : endDate;
+            
+            var books = _context.Books.Include(x => x.Category).Include(m => m.Marks).ToList();
+
+            books = books.Where(x => x.BookName.ToLower().Contains(bookName.ToLower())).ToList();
+
+            if (startRating != 0)
+            {
+                books = books.Where(x =>
+                    x.Marks.Any() && x.Marks.Average(k => k.Mark) >= startRating &&
+                    x.Marks.Average(k => k.Mark) <= endRating).ToList();
+            }
+
+            books = books.Where(e => e.PublicationDate >= startDate && e.PublicationDate <= endDate).ToList();
+
+            return View(books);
         }
     }
 }
